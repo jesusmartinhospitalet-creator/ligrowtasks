88a5ae5febea135a8589204158033f60aa52c6f9
jesusmartinhospitalet-creator <jesus.martin.hospitalet@gmail.com>
2026-09-05 13:30:21 +0200

    debug(vercel): capture and expose serverless boot stack traces in JSON

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
-const app = require('../src/server');
+module.exports = (req, res) => {
+  try {
+    const app = require('../src/server');
+    return app(req, res);
+  } catch (err) {
+    console.error('[SERVERLESS BOOT ERROR]', err);
+    res.status(500).json({
+      error: 'Serverless initialization error',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
 
-module.exports = app;
 
 
